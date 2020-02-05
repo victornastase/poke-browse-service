6d8b589993ef808133d193984880241f73fb4fa4
Victor Nastase <victor.gabriel.nastase@gmail.com>
2020-02-05 22:22:06 +0200

    Generated project

---
+++ src/main/java/com/pokedex/microservice/pokebrowseservice/PokeBrowseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pokedex.microservice.pokebrowseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeBrowseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeBrowseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokedex/microservice/pokebrowseservice/PokeBrowseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokedex.microservice.pokebrowseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeBrowseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
